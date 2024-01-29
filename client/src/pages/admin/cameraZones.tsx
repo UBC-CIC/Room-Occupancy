2aa75f1f105e55c1b729f9139f0d5484dcbfcf02
CassielJung <eunjijung1101@gmail.com>
2024-01-28 17:22:04 -0800

    add basic structure for camera zone settings page

--- client/src/pages/admin/cameraZones.tsx
+++ client/src/pages/admin/cameraZones.tsx
@@ -1,25 +1,64 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import React from "react";
 import { AdminDashboardLayout } from "../../components/layout/admin";
-import { Header, HeaderSubheader } from "semantic-ui-react";
+import { useGetCamList } from "../../shared/hooks/useGetCamList";
+import { CameraZoneSettings } from "./cameraZoneSettings";
+import {
+  Header,
+  HeaderSubheader,
+  Segment,
+  Dimmer,
+  Loader,
+  Image,
+  Tab,
+} from "semantic-ui-react";
 
 type Props = {};
 
-const cameraZonesComponent = (props: Props) => {
-  return (
-    <AdminDashboardLayout>
-      <Header as="h2">
-        Camera Zones
-        <HeaderSubheader>
-          View and update camera zone information
-        </HeaderSubheader>
-      </Header>
-      <br />
-      Camera Zones... TBD Cassy
-    </AdminDashboardLayout>
-  );
+interface Pane {
+  menuItem: { key: string; icon: string; content: string };
+  render: () => React.ReactNode;
+}
+
+const CameraZonesComponent = (props: Props) => {
+  const { camList } = useGetCamList();
+
+  const panes: Pane[] = camList.map((camera: any) => ({
+    menuItem: { key: camera.id, icon: 'camera', content: camera.name },
+    render: () => <CameraZoneSettings camera={camera} />, // Assuming you have a CameraSettings component
+  }));
+
+  if (camList.length === 0)
+    return (
+      <AdminDashboardLayout>
+        <Header as="h2">
+          Camera Zones
+          <HeaderSubheader>
+            View and update camera zone information
+          </HeaderSubheader>
+        </Header>
+        <Segment>
+          <Dimmer active inverted>
+            <Loader inverted content="Fetching Camera Data" />
+          </Dimmer>
+          <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+        </Segment>
+      </AdminDashboardLayout>
+    );
+  else
+    return (
+      <AdminDashboardLayout>
+        <Header as="h2">
+          Camera Zones
+          <HeaderSubheader>
+            View and update camera zone information
+          </HeaderSubheader>
+        </Header>
+        <Tab panes={panes} />
+      </AdminDashboardLayout>
+    );
 };
 
-export const CameraZones = withAuthenticator(cameraZonesComponent, {
+export const CameraZones = withAuthenticator(CameraZonesComponent, {
   hideSignUp: true,
 });
